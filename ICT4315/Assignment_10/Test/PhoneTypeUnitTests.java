652f44b8e557c7dcf5e191274c827d18378e8026
Frederick McGovern <Frederick_McGovern@msn.com>
2017-06-24 12:28:36 -0400

    Create PhoneTypeUnitTests.java

---
+++ ICT4315/Assignment_10/Test/PhoneTypeUnitTests.java
@@ -0,0 +1,76 @@
+package ict4315.assignment10;
+
+import static org.junit.Assert.*;
+import ict4315.assignment10.HelperClasses.PhoneType;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PhoneTypeUnitTests {
+	
+	
+
+	@Before
+	public void setUp() throws Exception 
+	{
+		PhoneType.getValues();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testGetSizeOfArrayShouldEqualFourExpectTrue() 
+	{
+		//Assert
+		assertEquals("The wrong array size was returned.  Size should have been four", PhoneType.size(), 4);
+	}
+
+	@Test
+	public void testCheckThatStringReturnedValidIndex0ReturnsValueHome() 
+	{
+		//Arrange
+		String returnedValue;
+		returnedValue = PhoneType.stringAtIndex(0);
+		
+		//Assert
+		assertEquals("The wrong value was returned.  The value should have been 'Home'", returnedValue, "Home");
+	}
+	
+	@Test
+	public void testCheckThatStringReturnedInvalidIndex4ReturnsValueInvalid() 
+	{
+		//Arrange
+		String returnedValue;
+		returnedValue = PhoneType.stringAtIndex(4);
+		
+		//Assert
+		assertEquals("The wrong value was returned.  The value should have been 'Invalid'", returnedValue, "Invalid");
+	}
+	
+	@Test
+	public void testCheckThatIntReturnedForItemInListIsValid() 
+	{
+		//Arrange
+		int returnedValue;
+		returnedValue = PhoneType.indexOf("Home");
+		
+		//Assert
+		assertEquals("The wrong value was returned.  The value should have been '0'", returnedValue, 0);
+	}
+	
+	@Test
+	public void testCheckThatIntReturnedForItemInListIsInvalid() 
+	{
+		//Arrange
+		int returnedValue;
+		returnedValue = PhoneType.indexOf("Crocodile");
+		
+		//Assert
+		assertEquals("The wrong value was returned.  The value should have been '-1'", returnedValue, -1);
+	}
+
+
+}
