907238fc3239396e21aa37f1f9af0163eba15c76
Frederick McGovern <Frederick_McGovern@msn.com>
2017-06-24 12:15:55 -0400

    Create XMLFileStrategy.java
    Part of ICT 4315 Assignment 10:
    
    This is the concrete implementation of the CreateFileOutput abstract class.
    This class provides a concrete implementation of the writeContact method which is used to write a contact to a xml file.

---
+++ ICT4315/Assignment_10/OutputStrategies/XMLFileStrategy.java
@@ -0,0 +1,54 @@
+package ict4315.assignment10.OutputStrategies;
+
+
+import ict4315.assignment10.BaseClasses.Contact;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+/**
+* @author Frederick McGovern
+* @version 1.3
+* @since 2014-05-17
+* 
+* This is the concrete implementation of the CreateFileOutput abstract class.
+* This class provides a concrete implementation of the writeContact method
+* which is used to write a contact to a xml file.
+*/
+
+
+public class XMLFileStrategy extends CreateFileOutput
+{
+
+	/**
+     * Method to add a contact to an XML file using XML formatting 
+	 * @return 
+     */
+	public void writeContact(Contact c, String fileLoc)
+	{
+		BufferedWriter bw = openWriter(fileLoc + ".xml");
+		
+		try
+		{
+			bw.write("<Contact Book>\r\n");
+			bw.write("  <Contact>\r\n");
+			bw.write("    <Full Name>" + c.getFullName() + "</Full Name>\r\n");
+			bw.write("    <Street>" + c.getStreet() + "</Street>\r\n");
+			bw.write("    <City>" + c.getCity() + "</City>\r\n");
+			bw.write("    <State>" + c.getState() + "</State>\r\n");
+			bw.write("    <Zip Code>" + c.getZipCode() + "</Zip Code>\r\n");
+			bw.write("    <Phone Number>" + c.getPhone().getPhoneNumber() + "</Phone Number>\r\n");
+			bw.write("    <Phone Type>" + c.getPhone().getPhoneType() + "</Phone Type>\r\n");
+			bw.write("    <EMail>" + c.getEmail() + "</EMail>\r\n");
+			bw.write("  </Contact>\r\n");
+			bw.write("</Contact Book>\r\n");
+		}
+		catch(IOException e)
+		{
+			System.out.println("An error has occurred while attempting to write to the file");
+			e.printStackTrace();
+		}
+		
+		closeWriter(bw);
+	}
+}
